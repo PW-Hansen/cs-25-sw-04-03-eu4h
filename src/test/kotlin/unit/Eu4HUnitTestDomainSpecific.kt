1e36a00fc43a772338e18168f6f11020d0ea01d4
Magnive <Magni-ps@Hotmail.com>
2025-05-26 20:46:59 +0200

    test fix

--- src/test/kotlin/unit/Eu4HUnitTestDomainSpecific.kt
+++ src/test/kotlin/unit/Eu4HUnitTestDomainSpecific.kt
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.sw_08.eu4h.abstract_syntax.*
 import org.sw_08.eu4h.interpretation.*
 
